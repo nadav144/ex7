e307c7019a881cf6a90c6dfd2ea8481eeed2ffe4
unknown <jirsch@gmail.com>
2014-06-15 17:31:22 +0300

    Negative integer assignment

--- src/oop/ex7/commands/ExpressionFactory.java
+++ src/oop/ex7/commands/ExpressionFactory.java
@@ -68,7 +68,7 @@ public class ExpressionFactory {
 					RegexUtils.MatchSignle( RegexUtils.DOUBLE_LITERAL_PATTERN,
 							expression );
 			try {
-				Integer.parseInt( result.group().trim() );
+				Integer.parseInt( result.group(1).trim() );
 				return new LiteralExpression( new TermType(
 						TermType.VarType.INT ) );
 			}
